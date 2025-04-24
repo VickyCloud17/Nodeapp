f9c942adba59240696b3b7031a854e1f13c0ca03
vicky <vicky@gmail.com>
2025-04-24 19:08:02 +0000

    last commit

--- test/test.js
+++ test/test.js
@@ -1,18 +1,10 @@
 const request = require('supertest');
+const app = require('../app'); // This should NOT call app.listen()
 
-const app = require('../index.js'); // Adjust if your app filename is app.js
-
-
-describe('GET /nonexistentpage', function() {
-
-  it('should respond with 404 Not Found', function(done) {
-
+describe('GET /nonexistentpage', function () {
+  it('should respond with 404 Not Found', function (done) {
     request(app)
-
       .get('/nonexistentpage')
-
-      .expect(404, done); // This is a cleaner way to end test if only checking status
-
+      .expect(404, done);
   });
-
 });
