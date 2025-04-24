84702bc7d0c868700a7850fad517c4abb2249657
vicky <vicky@gmail.com>
2025-04-24 19:01:06 +0000

    node commit3

--- test/test.js
+++ test/test.js
@@ -1,11 +1,18 @@
 const request = require('supertest');
-const app = require('../index.js'); // make sure this points to the Express app, not server
 
-describe('GET /nonexistentpage', function () {
-  it('should respond with 404 Not Found', function (done) {
+const app = require('../index.js'); // Adjust if your app filename is app.js
+
+
+describe('GET /nonexistentpage', function() {
+
+  it('should respond with 404 Not Found', function(done) {
+
     request(app)
-      .get('/nonexistentpage')
-      .expect(404, done);
-  });
-});
 
+      .get('/nonexistentpage')
+
+      .expect(404, done); // This is a cleaner way to end test if only checking status
+
+  });
+
+});
